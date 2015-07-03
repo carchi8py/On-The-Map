031a507e20f2b081ef0caa417a38427d31b7de21
Chris Archibald <carchi8py@gmail.com>
2015-07-03 14:58:20 -0700

    Couldn't get Reachability so i'm to not check if they have a networking running or not.

--- OnTheMap/OTMClient.swift
+++ OnTheMap/OTMClient.swift
@@ -9,10 +9,5 @@
 import Foundation
 
 class OTMClient : NSObject {
-    
-    // This function return true if we have network access.
-    // Rachability is a class from Apple Developer Libaray
-    //https://developer.apple.com/library/ios/samplecode/Reachability/Introduction/Intro.html#//apple_ref/doc/uid/DTS40007324-Intro-DontLinkElementID_2
-    class func isNetworkAccessible() -> Bool {
-    }
+
 }
