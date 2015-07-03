3f940b46f4ae8389d5fd85532e28fd4e443005ce
Chris Archibald <carchi8py@gmail.com>
2015-07-03 13:54:50 -0700

    Renamed the view controller.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheMap
 //
 //  Created by Chris Archibald on 7/3/15.
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
